c65d4d77837458d31ee803670811274251467ad5
Al-Jami Ismail <al-jami@Al-Jamis-MacBook-Pro.local>
2017-04-16 13:26:48 -0400

    changed android back button behaviour

--- android/app/src/main/java/com/airohealth/MainActivity.java
+++ android/app/src/main/java/com/airohealth/MainActivity.java
@@ -16,8 +16,13 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
-		public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		    super.onActivityResult(requestCode, resultCode, data);
-		    MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
-		}
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+	}
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
 }
